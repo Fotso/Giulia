ed38aa219f931e075b56760df6c63d64a7511dee
Fotso <fotso.theophane@gmail.com>
2022-12-26 16:05:16 +0100

    Initial Commit

---
+++ Guilia/GuiliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuiliaApp.swift
+//  Guilia
+//
+//  Created by Theophane on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct GuiliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
